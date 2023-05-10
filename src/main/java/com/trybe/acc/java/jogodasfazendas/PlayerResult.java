ceb9090b2817e301f67783356033a9252e38c36b
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-05-10 18:37:09 -0300

    feat: class PlayerResult

--- src/main/java/com/trybe/acc/java/jogodasfazendas/PlayerResult.java
+++ src/main/java/com/trybe/acc/java/jogodasfazendas/PlayerResult.java
@@ -1,7 +1,33 @@
 package com.trybe.acc.java.jogodasfazendas;
 
+/**
+ * Classe que representa os resultados de um jogador no jogo das fazendas.
+ */
 public class PlayerResult {
+  String playerName;
+  Farm[] farms;
 
+  public PlayerResult(String name, Farm[] farms) {
+    this.playerName = name;
+    this.farms = farms;
+  }
 
+  /**
+   * Calcula a pontuação do jogador com base nas áreas das fazendas que possui.
+   *
+   * @return a pontuação do jogador
+   */
+  public double score() {
+    double score = 0.0;
 
+    for (Farm farm : this.farms) {
+      score += farm.area();
+    }
+
+    return score;
+  }
+
+  public String toString() {
+    return "";
+  }
 }
